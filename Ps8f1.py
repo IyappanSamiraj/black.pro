bcf97a368ee46cb6cf24d1f740bc5e1375c21357
iyappan10 <52095660+iyappan10@users.noreply.github.com>
2019-08-16 09:17:27 +0530

    Create Ps8f1.py

---
+++ Ps8f1.py
@@ -0,0 +1,24 @@
+n,m=map(int,input().split())
+num=list(map(int,input().split()))
+for i in range(m):
+    u,v=map(int,input().split())
+    if(u==v):
+        print(num[u-1])
+    else:
+        u=u-1
+        v=v-1
+        if(num[u]>num[v]):
+            if(num[u]%num[v]==1):
+                f=num[u]%num[v]
+                print(f)
+            else:
+                f=num[v]%num[u]
+                print(f)            
+        else:
+            if(num[v]%num[u]==1):
+                f=num[v]%num[u]
+                print(f)
+            else:
+                f=num[u]%num[v]
+                print(f)
+                
